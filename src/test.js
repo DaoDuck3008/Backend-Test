d0f45be6c2d7fb320182977b761649d448eceab3
Dao Duck <dduccraft@gmail.com>
2024-12-14 11:49:22 +0700

    tai su dung bien connection trong test.js

--- src/test.js
+++ src/test.js
@@ -4,7 +4,7 @@ const configViewEngine = require('./config/viewEngine.js');
 const WebRoutes = require('./routes/web.js');
 const app = express()
 const port = process.env.PORT || 8080;
-const mysql = require('mysql2');
+const connection = require('./config/database.js');
 
 //config
 configViewEngine(app);
@@ -12,20 +12,13 @@ configViewEngine(app);
 //Khai bao router
 app.use('/v1', WebRoutes);
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307,
-  user: 'root',
-  password: '123456',
-  database: 'hoidanit'
-});
+
 
 //simple query
 connection.query(
   'SELECT * FROM Users',
   function (err, results, fields){
     console.log(">>> results= ", results);
-    console.log(">>> fields=", fields);
   }
 );
 
