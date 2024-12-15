47d37a42c083631547495977ab9be88428bfa592
Dao Duck <dduccraft@gmail.com>
2024-12-15 12:29:59 +0700

    Add Horizontal NavBar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,16 +2,7 @@
 const connection = require('../config/database.js');
 
 const getHomePage = (req,res) => {
-    let users = [];
-    connection.query(
-        'SELECT * FROM Users',
-        function (err, results, fields){
-          users = results;
-          console.log(">>> results= ", results);
-          console.log(">>> users= ",users);
-          res.send(JSON.stringify(users));
-        }
-      );
+    return res.render('home.ejs');
 }
 
 const getABC = (req,res) => {
